a1333876030c60397fbb6f7a5059ec27f002a3af
Francisco Bernardo <f.bernardo@gold.ac.uk>
2018-01-12 12:55:40 +0000

    classification tested successful

--- extension.js
+++ extension.js
@@ -26,9 +26,6 @@
       console.log('Loaded RapidLib Modules');
     }
 
-
-
-
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -50,33 +47,21 @@
       myTrainingSet = [];
     };
 
-
     ext.trainClassifier = function(f1, f2, f3, f4, f5, label) {
-      // myTrainingSet.push({
-      //   input: [f1, f2, f3, f4, f5],
-      //   output: [label]
-      // });
       classification.train(myTrainingSet);
     };
 
     ext.runClassifier = function(f1, f2, f3, f4, f5) {
-      // return classification.run([f1, f2, f3, f4, f5]);
       return classification.run([f1, f2, f3, f4, f5]);
     };
 
     ext.trainRegressifier = function(f1, f2, f3, f4, f5, label) {
-      // myTrainingSet.push({
-      //   input: [f1, f2, f3, f4, f5],
-      //   output: [label]
-      // });
       regression.train(myTrainingSet);
     };
 
     ext.runRegressifier = function(f1, f2, f3, f4, f5) {
-      // return regression.run([f1, f2, f3, f4, f5]);
       return regression.run([f1, f2, f3, f4, f5]);
     };
-
 
     // Block and block menu descriptions
     var descriptor = {
@@ -90,11 +75,6 @@
         ['-'],
         ['w', 'train regressifier', 'trainRegressifier'],
         ['r', 'run regression f1 %n f2 %n f3 %n f4 %n f5 %n', 'runRegressifier', 0, 0, 0, 0, 0],
-        // ['r', '%n ^ %n', 'power', 2, 3],
-
-        // ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
-        // ['', 'run alarm after %n seconds', 'set_alarm', '2'],
-        // ['h', 'when alarm goes off', 'when_alarm']
       ],
       url: 'https://frantic0.github.io/scratch-interactive-machine-learning-extension/'
     };
