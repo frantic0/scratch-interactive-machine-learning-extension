b67791e302c1d32b6d0239ddf57dd71602c2ca71
Francisco Bernardo <f.bernardo@gold.ac.uk>
2018-01-12 10:52:29 +0000

    wtf2

--- extension.js
+++ extension.js
@@ -7,43 +7,36 @@
 
   // Cleanup function when the extension is unloaded
   ext._shutdown = function() {
-    rapidLib = null;
-    regression = null;
-    classification = null;
-    myTrainingSet = [];
+    // rapidLib = null;
+    // regression = null;
+    // classification = null;
+    // myTrainingSet = [];
   };
-
   // Status reporting code
   // Use this to report missing hardware, plugin or unsupported browser
   ext._getStatus = function() {
-      return {status: 2, msg: 'Ready'};
+    return {status: 2, msg: 'Ready'};
   };
-
-  function loadRapidLib() {
-    console.log('Loading RapidLib');
-    // $.getScript('https://www.doc.gold.ac.uk/eavi/rapidmix/RapidLib.js')
-    //   .done(function(script, textStatus) {
-    //     console.log('Loaded RapidLib');
-    //     // loadRapidLibModules();
-    //   })
-    //   .fail(function(jqxhr, settings, exception) {
-    //     console.log('Error loading RapidLib');
-    //     loadRapidLib();
-    // });
-  }
-
+  // function loadRapidLib() {
+  //   console.log('Loading RapidLib');
+  //   // $.getScript('https://www.doc.gold.ac.uk/eavi/rapidmix/RapidLib.js')
+  //   //   .done(function(script, textStatus) {
+  //   //     console.log('Loaded RapidLib');
+  //   //     // loadRapidLibModules();
+  //   //   })
+  //   //   .fail(function(jqxhr, settings, exception) {
+  //   //     console.log('Error loading RapidLib');
+  //   //     loadRapidLib();
+  //   // });
+  // }
   // function loadRapidLibModules() {
   //   rapidLib = window.RapidLib();
   //   regression = new rapidLib.Regression();
   //   classification = new rapidLib.Classification();
   // }
-
-
-
   ext.my_first_block = function() {
       // Code that gets executed when the block is run
   };
-
   // Block and block menu descriptions
   var descriptor = {
       blocks: [
@@ -51,7 +44,6 @@
           [' ', 'my first block', 'my_first_block'],
       ]
   };
-
   // Register the extension
   ScratchExtensions.register('My first extension', descriptor, ext);
   loadRapidLib();
