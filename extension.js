b07d1c03a39e596535a5f728b01ab5c6486824b9
Francisco Bernardo <f.bernardo@gold.ac.uk>
2018-01-12 10:38:26 +0000

    testing loadRapidLib and loadRapidLibModules

--- extension.js
+++ extension.js
@@ -1,25 +1,51 @@
 (function(ext) {
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
 
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
+  var rapidLib = null;
+  var regression = null;
+  var classification = null;
+  var myTrainingSet = [];
 
-    ext.my_first_block = function() {
-        // Code that gets executed when the block is run
-    };
+  // Cleanup function when the extension is unloaded
+  ext._shutdown = function() {
+    return {status: 2, msg: 'Ready'};
+  };
 
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            // Block type, block name, function name
-            [' ', 'my first block', 'my_first_block'],
-        ]
-    };
+  function loadRapidLib() {
+    $.getScript('https://www.doc.gold.ac.uk/eavi/rapidmix/RapidLib.js')
+      .done(function(script, textStatus) {
+        console.log('Loaded RapidLib');
+        loadRapidLibModules();
+      })
+      .fail(function(jqxhr, settings, exception) {
+        console.log('Error loading RapidLib');
+        loadRapidLib();
+    });
+  }
 
-    // Register the extension
-    ScratchExtensions.register('My first extension', descriptor, ext);
+  function loadRapidLibModules() {
+    rapidLib = window.RapidLib();
+    regression = new rapidLib.Regression();
+    classification = new rapidLib.Classification();
+  }
+
+  // Status reporting code
+  // Use this to report missing hardware, plugin or unsupported browser
+  ext._getStatus = function() {
+      return {status: 2, msg: 'Ready'};
+  };
+
+  ext.my_first_block = function() {
+      // Code that gets executed when the block is run
+  };
+
+  // Block and block menu descriptions
+  var descriptor = {
+      blocks: [
+          // Block type, block name, function name
+          [' ', 'my first block', 'my_first_block'],
+      ]
+  };
+
+  // Register the extension
+  ScratchExtensions.register('My first extension', descriptor, ext);
 })({});
