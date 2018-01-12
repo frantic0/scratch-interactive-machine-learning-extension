df322898374762b2fb0573aae414c87bc88730bb
Francisco Bernardo <f.bernardo@gold.ac.uk>
2018-01-12 11:37:54 +0000

    on track again

--- extension.js
+++ extension.js
@@ -23,6 +23,7 @@
       rapidLib = window.RapidLib();
       regression = new rapidLib.Regression();
       classification = new rapidLib.Classification();
+      console.log('Loaded RapidLib Modules');
     }
 
     ext.trainClassifier = function(f1, f2, f3, f4, f5, label) {
@@ -61,50 +62,50 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.power = function(base, exponent) {
-        return Math.pow(base, exponent);
-    };
-
-    // Functions for block with type 'w' will get a callback function as the
-    // final argument. This should be called to indicate that the block can
-    // stop waiting.
-    ext.wait_random = function(callback) {
-        wait = Math.random();
-        console.log('Waiting for ' + wait + ' seconds');
-        window.setTimeout(function() {
-            callback();
-        }, wait*1000);
-    };
-
-    ext.get_temp = function(location, callback) {
-            // Make an AJAX call to the Open Weather Maps API
-            $.ajax({
-                  url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
-                  dataType: 'jsonp',
-                  success: function( weather_data ) {
-                      // Got the data - parse it and return the temperature
-                      temperature = weather_data['main']['temp'];
-                      callback(temperature);
-                  }
-            });
-        };
-
-        ext.set_alarm = function(time) {
-       window.setTimeout(function() {
-           alarm_went_off = true;
-       }, time*1000);
-    };
-
-    ext.when_alarm = function() {
-      // Reset alarm_went_off if it is true, and return true
-      // otherwise, return false.
-      if (alarm_went_off === true) {
-         alarm_went_off = false;
-         return true;
-      }
-
-      return false;
-    };
+    // ext.power = function(base, exponent) {
+    //     return Math.pow(base, exponent);
+    // };
+    //
+    // // Functions for block with type 'w' will get a callback function as the
+    // // final argument. This should be called to indicate that the block can
+    // // stop waiting.
+    // ext.wait_random = function(callback) {
+    //     wait = Math.random();
+    //     console.log('Waiting for ' + wait + ' seconds');
+    //     window.setTimeout(function() {
+    //         callback();
+    //     }, wait*1000);
+    // };
+    //
+    // ext.get_temp = function(location, callback) {
+    //         // Make an AJAX call to the Open Weather Maps API
+    //         $.ajax({
+    //               url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
+    //               dataType: 'jsonp',
+    //               success: function( weather_data ) {
+    //                   // Got the data - parse it and return the temperature
+    //                   temperature = weather_data['main']['temp'];
+    //                   callback(temperature);
+    //               }
+    //         });
+    //     };
+    //
+    //     ext.set_alarm = function(time) {
+    //    window.setTimeout(function() {
+    //        alarm_went_off = true;
+    //    }, time*1000);
+    // };
+    //
+    // ext.when_alarm = function() {
+    //   // Reset alarm_went_off if it is true, and return true
+    //   // otherwise, return false.
+    //   if (alarm_went_off === true) {
+    //      alarm_went_off = false;
+    //      return true;
+    //   }
+    //
+    //   return false;
+    // };
 
 
     // Block and block menu descriptions
@@ -116,11 +117,11 @@
         ['-'],
         [' ', 'train Regression f1 %n f2 %n f3 %n f4 %n f5 %n i0 %n i1 %n', 'trainRegressifier', 0, 0, 0, 0, 0, 0, 0],
         ['r', 'run Regression f1 %n f2 %n f3 %n f4 %n f5 %n', 'runRegressifier', 0, 0, 0, 0, 0],
-        ['r', '%n ^ %n', 'power', 2, 3],
-        ['w', 'wait for random time', 'wait_random'],
-        ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
-        ['', 'run alarm after %n seconds', 'set_alarm', '2'],
-        ['h', 'when alarm goes off', 'when_alarm']
+        // ['r', '%n ^ %n', 'power', 2, 3],
+        // ['w', 'wait for random time', 'wait_random'],
+        // ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
+        // ['', 'run alarm after %n seconds', 'set_alarm', '2'],
+        // ['h', 'when alarm goes off', 'when_alarm']
       ],
       url: 'https://frantic0.github.io/scratch-interactive-machine-learning-extension/'
     };
