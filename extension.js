2032f4dcf599318f9312bb937441fb6c0d924a7a
Francisco Bernardo <f.bernardo@gold.ac.uk>
2018-01-12 09:53:09 +0000

    test 120102

--- extension.js
+++ extension.js
@@ -39,39 +39,42 @@
       classification = new rapidLib.Classification();
     }
 
-    // ext.trainClassifier = function(f1, f2, f3, f4, f5, label) {
-    //   myTrainingSet.push({
-    //     input: [f1, f2, f3, f4, f5],
-    //     output: [label]
-    //   });
-    //   classification.train(myTrainingSet);
-    // };
-    //
-    // ext.runClassifier = function(f1, f2, f3, f4, f5) {
-    //   return classification.run([f1, f2, f3, f4, f5]);
-    // };
-    //
-    // ext.trainRegressifier = function(f1, f2, f3, f4, f5, i0, i2) {
-    //   myTrainingSet.push({
-    //     input: [f1, f2, f3, f4, f5],
-    //     output: [label]
-    //   });
-    //   regression.train(myTrainingSet);
-    // };
-    //
-    // ext.runRegressifier = function(f1, f2, f3, f4, f5) {
-    //   return regression.run([f1, f2, f3, f4, f5]);
-    // };
+    ext.trainClassifier = function(f1, f2, f3, f4, f5, label) {
+      // myTrainingSet.push({
+      //   input: [f1, f2, f3, f4, f5],
+      //   output: [label]
+      // });
+      // classification.train(myTrainingSet);
+    };
+
+    ext.runClassifier = function(f1, f2, f3, f4, f5) {
+      // return classification.run([f1, f2, f3, f4, f5]);
+    };
+
+    ext.trainRegressifier = function(f1, f2, f3, f4, f5, i0, i2) {
+      // myTrainingSet.push({
+      //   input: [f1, f2, f3, f4, f5],
+      //   output: [label]
+      // });
+      // regression.train(myTrainingSet);
+    };
+
+    ext.runRegressifier = function(f1, f2, f3, f4, f5) {
+      // return regression.run([f1, f2, f3, f4, f5]);
+    };
 
     var descriptor = {
       blocks: [
         [' ', 'train Classifier f1 %n f2 %n f3 %n f4 %n f5 %n Label %n', 'trainClassifier', 0, 0, 0, 0, 0, 0],
         ['r', 'run Classifier f1 %n f2 %n f3 %n f4 %n f5 %n', 'runClassifier', 0, 0, 0, 0, 0],
+        // ['w', 'run Classifier f1 %n f2 %n f3 %n f4 %n f5 %n', 'runClassifier', 0, 0, 0, 0, 0],
         ['-'],
         [' ', 'train Regression f1 %n f2 %n f3 %n f4 %n f5 %n i0 %n i1 %n', 'trainRegressifier', 0, 0, 0, 0, 0, 0, 0],
         ['r', 'run Regression f1 %n f2 %n f3 %n f4 %n f5 %n', 'runRegressifier', 0, 0, 0, 0, 0]
       ],
-      url: 'http://frantic0.github.io/scratch-interactive-machine-learning-extension'
+      menus: null,
+      url: 'http://frantic0.github.io/scratch-interactive-machine-learning-extension',
+      displayName: 'Interactive Machine Learning'
     };
 
     // Register the extension
